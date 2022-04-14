4c2f3e80fb583dba63db860670d515a4bdcad5b8
EdRma <34298@ma-web.nl>
2022-04-14 10:41:53 +0200

    afgemaakt

--- les6_overerving/les6_overerving/Program.cs
+++ les6_overerving/les6_overerving/Program.cs
@@ -2,51 +2,24 @@
 
 namespace les6_overerving
 {
-        internal class Program
+    class Animal
+    {
+        public void feed()
         {
-            static void Main(string[] args)
-            {
-                Animal whitehorse = new Horse();
-                Animal blackhorse = new Horse();
-                whitehorse.Feed(5);
-            }
-        }
-        class Animal
-        {
-            protected int hunger
-            {
-                get
-                {
-                    return _hunger;
-                }
-                set
-                {
-                    if (value > hungermax)
-                        value = hungermax;
-                    _hunger = value;
-                }
-            }
-            private int _hunger;
-            protected int hungermax
-            {
-                get; set;
-            }
-            public void Feed(int v)
-            {
-                hunger -= 1;
-            }
-            public void Starve(int ammount)
-            {
-                hunger -= ammount;
-            }
-        }
-        class Horse : Animal
-        {
-            public Horse()
-            {
-                hungermax = 100;
-                hunger = 100;
-            }
+            Console.WriteLine("feed :)");
         }
     }
+    class horse : Animal
+    {
+        public string ras = "pony";
+    }
+    class horsenom
+    {
+        static void Main(string[] args)
+        {
+            horse myanimal = new horse();
+            myanimal.feed();
+        }
 
+    }
+}
