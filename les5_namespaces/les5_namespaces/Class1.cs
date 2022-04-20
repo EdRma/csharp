14143b5ed04e2013afea4f8cf24392a95c18ebf8
EdRma <34298@ma-web.nl>
2022-04-20 10:10:27 +0200

    fix

---
+++ les5_namespaces/les5_namespaces/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les5_namespaces
+{
+    class Class1
+    {
+        private int peop;
+
+        public void Feed(int amount)
+        {
+            peop -= amount;
+            Console.WriteLine(peop);
+        }
+    }
+}
---
+++ les5_namespaces/les5_namespaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace les5_namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
