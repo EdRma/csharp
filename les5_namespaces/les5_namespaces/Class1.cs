b42afdadd7a2b583f67fc2f84b78f15642efe826
EdRma <34298@ma-web.nl>
2022-04-21 11:27:11 +0200

    af

--- les5_namespaces/les5_namespaces/Class1.cs
+++ les5_namespaces/les5_namespaces/Class1.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace les5_namespaces
+namespace Namespace_1
 {
-    class Class1
-    {
-        private int peop;
 
-        public void Feed(int amount)
+}
+namespace Namespace_2
+{
+    public class gebak
+    {
+
+    }
+}
+namespace Namespace_3
+{
+   class program
+   {
+        static void main()
         {
-            peop -= amount;
-            Console.WriteLine(peop);
+            Namespace_2.gebak snoep = new Namespace_2.gebak();
         }
     }
 }
